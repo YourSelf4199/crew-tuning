8907af5b4412d315d40f04d1c29d04b79bb5b3b5
Deaven Nortier <deaven00@gmail.com>
2025-04-12 16:56:54 +0200

    Fixed define storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,9 +3,9 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'vehicleAssets',
   access: (allow) => ({
-    'vehicles/street-race/*': [allow.authenticated.to(['read'])],
-    'vehicles/off-road/*': [allow.authenticated.to(['read'])],
-    'vehicles/freestyle/*': [allow.authenticated.to(['read'])],
-    'vehicles/pro-racing/*': [allow.authenticated.to(['read'])],
+    'street-race/*': [allow.authenticated.to(['read'])],
+    'off-road/*': [allow.authenticated.to(['read'])],
+    'freestyle/*': [allow.authenticated.to(['read'])],
+    'pro-racing/*': [allow.authenticated.to(['read'])],
   }),
 });
