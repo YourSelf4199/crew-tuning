1d84f4e5abb56416ebeb3209dfb0d53b74682a9c
Deaven Nortier <deaven00@gmail.com>
2025-04-13 21:40:00 +0200

    Tried fixing 403 token expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectIsTokenExpired } from './store/auth/auth.selectors';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'crew-tuning';
+export class AppComponent implements OnInit {
+  constructor(
+    private store: Store,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    this.store.select(selectIsTokenExpired).subscribe((isExpired) => {
+      if (isExpired) {
+        this.authService.setIdToken();
+      }
+    });
+  }
 }
--- src/app/store/auth/auth.selectors.ts
+++ src/app/store/auth/auth.selectors.ts
@@ -1,6 +1,7 @@
 // auth.selectors.ts
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AuthState } from './auth.reducer';
+import { jwtDecode } from 'jwt-decode';
 
 // Feature selector to select the auth state
 export const selectAuthState = createFeatureSelector<AuthState>('auth');
@@ -14,3 +15,15 @@ export const selectIdToken = createSelector(
 );
 
 export const selectUserId = createSelector(selectAuthStateFull, (state: AuthState) => state.userId);
+
+export const selectIsTokenExpired = createSelector(selectIdToken, (token) => {
+  if (!token) return true;
+  try {
+    const payload: any = jwtDecode(token);
+
+    const expiry = payload.exp * 1000;
+    return Date.now() >= expiry;
+  } catch {
+    return true;
+  }
+});
