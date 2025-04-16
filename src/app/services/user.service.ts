bb85d00b1436febb874a6896199b9917b1744d01
Deaven Nortier <deaven00@gmail.com>
2025-04-16 21:34:52 +0200

    new user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private apollo: Apollo) {}
+
+  /**
+   * Save user to Hasura database
+   */
+  async saveUserToHasura(userId: string, email: string, name: string) {
+    const SAVE_USER = gql`
+      mutation InsertUser($email: String!, $sub: String!, $name: String) {
+        insert_users_one(
+          object: { email: $email, cognito_sub: $sub, name: $name }
+          on_conflict: { constraint: users_cognito_sub_key, update_columns: [] }
+        ) {
+          id
+        }
+      }
+    `;
+
+    try {
+      await this.apollo
+        .mutate({
+          mutation: SAVE_USER,
+          variables: {
+            email,
+            sub: userId,
+            name,
+          },
+        })
+        .toPromise();
+    } catch (error) {
+      console.error('Failed to save user to Hasura:', error);
+      throw error;
+    }
+  }
+}
